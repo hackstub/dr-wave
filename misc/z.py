ade48be32ed84f31bfbc9679ff37511ccbb900ff
Val <val@legtux.org>
2017-01-21 00:10:25 +0100

    Z

--- misc/z.py
+++ misc/z.py
@@ -14,10 +14,22 @@ n_samples = int(round(duration*sample_rate))
 buf = numpy.zeros((n_samples, 2), dtype = numpy.int16)
 max_sample = 2**(bits - 1) - 1
 
+def mod_amp_tri(t):
+	if t < 0.5: return t
+	else: return 1-t
+
+def mod_amp_sin(t):
+	return (math.sin(t*13))/2
+
+def fn_square(t):
+	if math.ceil(t)%2 == 0: return 1.0
+	else: return 0.0
+
 for s in range(n_samples):
 	t = float(s)/sample_rate        # time in seconds
-	buf[s][0] = int(round(max_sample*math.sin(2*math.pi*440*t))) # left
-	buf[s][1] = int(round(max_sample*0.5*math.sin(2*math.pi*440*t)))    # right
+	print(fn_square(t/2.0))
+	buf[s][0] = int(round(max_sample*math.sin(2*math.pi*440*t)*fn_square(t))) # left
+	buf[s][1] = buf[s][0]    # right
 
 sound = pygame.sndarray.make_sound(buf)
 sound.play()
