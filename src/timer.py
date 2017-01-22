005e0efe602765703f2e117b1027a091e6da96b1
Alexandre Aubin <alexandre.aubin@cern.ch>
2017-01-22 16:17:58 +0100

    Adding timer box

--- src/timer.py
+++ src/timer.py
@@ -6,11 +6,18 @@ class Timer() :
 
     def __init__(self) :
         self.t0 = pygame.time.get_ticks()
+        
+        self.box    = shared.assetsdb["timer_box"]
+        self.digits = shared.assetsdb["timer_digits"]
 
-    def update(self) : self.t = pygame.time.get_ticks()
+    def update(self) :
+    
+        self.t = pygame.time.get_ticks()
 
     def render(self) :
         t, dx = math.floor((self.t-self.t0)/100), 0
+        shared.game.screen.blit(self.box,     (shared.screenSize[0]-188, 12))
         for c in str(t):
-            shared.game.screen.blit(shared.assetsdb["timer_digits"].sprites[int(c)], (1280-24+dx-48*len(str(t)), 24))
+            shared.game.screen.blit(self.digits.sprites[int(c)],
+                    (shared.screenSize[0]-24+dx-48*len(str(t)), 34))
             dx += 48
