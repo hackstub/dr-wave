1411001cb554a31b598323bf631064d660752faa
Alexandre Aubin <alexandre.aubin@cern.ch>
2017-01-22 15:59:05 +0100

    Tweaking stuff in the menu

--- src/menuGraphics.py
+++ src/menuGraphics.py
@@ -5,6 +5,11 @@ import src.shared as shared
 class MenuGraphics() :
 
     def __init__(self) :
+
+        self.title = shared.assetsdb["title"]
+        self.play  = shared.assetsdb["menu_play"]
+        self.quit  = shared.assetsdb["menu_quit"]
+        
         pass
 
     def update(self) :
@@ -16,12 +21,12 @@ class MenuGraphics() :
 
         t = 1000*pygame.time.get_ticks()
         if idx == 1:
-            d1 = 40+4*math.sin(t)
+            d1 = 4*math.sin(t)
             d2 = 0
         else:
             d1 = 0
-            d2 = 40+4*math.sin(t)
+            d2 = 4*math.sin(t)
         
-        shared.game.screen.blit(shared.assetsdb["title"], (1280/2-400/2,20-dy))
-        shared.game.screen.blit(shared.assetsdb["menu_play"], (1280/2-300/2+d1,300))
-        shared.game.screen.blit(shared.assetsdb["menu_quit"], (1280/2-300/2+d2,500+dy))
+        shared.game.screen.blit(self.title, (shared.screenSize[0]/2-self.title.get_size()[0]/2,20-dy))
+        shared.game.screen.blit(self.play,  (shared.screenSize[0]/2-self.play .get_size()[0]/2+d1,300))
+        shared.game.screen.blit(self.quit,  (shared.screenSize[0]/2-self.quit .get_size()[0]/2+d2,500+dy))
